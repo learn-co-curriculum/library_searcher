3f45894ee24a27f28ff77be87972af38dee59d43
alexgriff <alexalexgriffith@gmail.com>
2017-01-18 16:46:29 -0500

    add method stubs

--- library.rb
+++ library.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+
 class Library
   attr_accessor :name, :book_cases
 
@@ -8,5 +9,20 @@ class Library
     @book_cases = library_props[:book_cases]
   end
 
+  def total_book_count
+    # TODO: write your code here
+  end
+
+  def all_fiction_titles_with_128_pages
+    # TODO: write your code here
+  end
+
+  def location_of_longest_book
+    # TODO: write your code here
+  end
+
+  def has_all_titles_in_collection?(titles)
+    # TODO: write your code here
+  end
 
 end
